2a8813693dbbe10c638ae552b47d7c9049847e05
i96514262-lgtm <i96514262@gmail.com>
2026-06-06 03:27:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const path = require('path');
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-let users = {}; 
-let timer = 20;
-
-// O'yin vaqti
-setInterval(() => {
-    timer--;
-    if (timer < 0) timer = 20;
-    io.emit('timer', timer);
-}, 1000);
-
-io.on('connection', (socket) => {
-    socket.on('login', (name) => {
-        users[socket.id] = { name: name, balance: 300000 };
-        socket.emit('auth', users[socket.id]);
-    });
-
-    socket.on('bet', (amt) => {
-        if (users[socket.id] && users[socket.id].balance >= amt) {
-            users[socket.id].balance -= amt;
-            socket.emit('update', users[socket.id].balance);
-        }
-    });
+app.get('/admin', (req, res) => {
+    res.send('<form method="POST" action="/admin-login"><input type="password" name="pass" placeholder="Parol"><button type="submit">Kirish</button></form>');
 });
 
-http.listen(process.env.PORT || 3000, () => console.log("Server ishga tushdi"));
+app.post('/admin-login', (req, res) => {
+    if (req.body.pass === "sizningparolingiz") {
+        res.send("<h1>Admin Panel</h1><p>Bu yerdan o'yinni boshqarishingiz mumkin.</p>");
+    } else {
+        res.send("Xato parol! <a href='/admin'>Qaytadan</a>");
+    }
+});
+
+app.listen(process.env.PORT || 3000);
