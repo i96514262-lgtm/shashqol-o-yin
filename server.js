53b18abc492750b5e78563a07553f5e9f2b160b6
i96514262-lgtm <i96514262@gmail.com>
2026-06-06 01:37:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,12 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-let players = [];
 let timer = 20;
 let isPlaying = false;
-
-// 5 ta Bot yaratish
-let bots = [
-    { id: 'bot1', name: 'Bot_Ali', balance: 1000000 },
-    { id: 'bot2', name: 'Bot_Vali', balance: 1000000 },
-    { id: 'bot3', name: 'Bot_Sardor', balance: 1000000 },
-    { id: 'bot4', name: 'Bot_Aziz', balance: 1000000 },
-    { id: 'bot5', name: 'Bot_Bek', balance: 1000000 }
-];
+let onlineCount = 1245; // Hozirgi onlayn
 
 io.on('connection', (socket) => {
-    socket.on('join', (user) => {
-        players.push({ id: socket.id, name: user.name, balance: 300000 });
-        io.emit('updatePlayers', players.length + bots.length);
-    });
+    socket.emit('timer', timer);
 
     socket.on('placeBet', (amount) => {
         if (!isPlaying) {
@@ -32,11 +20,8 @@ io.on('connection', (socket) => {
                 io.emit('timer', timer);
                 if (timer <= 0) {
                     clearInterval(countdown);
-                    // O'yin natijasini hisoblash (Soliq 2%)
                     let tax = amount * 0.02;
-                    let winnerGain = amount - tax;
-                    io.emit('gameResult', "O'yin tugadi! Yutuq: " + winnerGain);
-                    
+                    io.emit('gameResult', "O'yin tugadi! Soliq olindi: " + tax);
                     timer = 20;
                     isPlaying = false;
                 }
@@ -46,5 +31,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(process.env.PORT || 3000, () => {
-    console.log('Server 24/7 rejimida ishga tushdi!');
+    console.log('Server ishga tushdi!');
 });
