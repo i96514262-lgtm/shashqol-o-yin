5aeb8ef90b875c82310df45e4be6e9e44601dcfc
i96514262-lgtm <i96514262@gmail.com>
2026-06-06 01:27:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,41 +6,45 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 let players = [];
-let gameState = 'waiting'; // waiting, playing
 let timer = 20;
+let isPlaying = false;
+
+// 5 ta Bot yaratish
+let bots = [
+    { id: 'bot1', name: 'Bot_Ali', balance: 1000000 },
+    { id: 'bot2', name: 'Bot_Vali', balance: 1000000 },
+    { id: 'bot3', name: 'Bot_Sardor', balance: 1000000 },
+    { id: 'bot4', name: 'Bot_Aziz', balance: 1000000 },
+    { id: 'bot5', name: 'Bot_Bek', balance: 1000000 }
+];
 
 io.on('connection', (socket) => {
-    console.log('Yangi o\'yinchi kirdi: ' + socket.id);
-
-    socket.on('join', (name) => {
-        players.push({ id: socket.id, name: name, balance: 300000 });
-        io.emit('updatePlayers', players);
+    socket.on('join', (user) => {
+        players.push({ id: socket.id, name: user.name, balance: 300000 });
+        io.emit('updatePlayers', players.length + bots.length);
     });
 
-    // 20 soniyalik sanoq logikasi
     socket.on('placeBet', (amount) => {
-        if (gameState === 'waiting') {
-            gameState = 'playing';
+        if (!isPlaying) {
+            isPlaying = true;
             let countdown = setInterval(() => {
                 timer--;
                 io.emit('timer', timer);
                 if (timer <= 0) {
                     clearInterval(countdown);
-                    calculateGameResult();
+                    // O'yin natijasini hisoblash (Soliq 2%)
+                    let tax = amount * 0.02;
+                    let winnerGain = amount - tax;
+                    io.emit('gameResult', "O'yin tugadi! Yutuq: " + winnerGain);
+                    
                     timer = 20;
-                    gameState = 'waiting';
+                    isPlaying = false;
                 }
             }, 1000);
         }
     });
 });
 
-function calculateGameResult() {
-    // Bu yerda siz aytgan "3 urug'", "4 urug'", "Siyo" va h.k. mantiq bo'ladi
-    console.log("O'yin natijasi aniqlandi");
-    io.emit('gameOver', { status: 'result' });
-}
-
 http.listen(process.env.PORT || 3000, () => {
-    console.log('Server 3000-portda ishlamoqda');
+    console.log('Server 24/7 rejimida ishga tushdi!');
 });
